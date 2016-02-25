f0d9f6dca304d137d0c488980d3f1b13bee120e0
wenyun.zhao@ic-pay.com <ai930225>
2016-02-25 19:31:33 +0800

    2016年2月25日19:31:22 all

--- smallrecipe/src/main/java/com/cn/smallrecipe/activity/LoginActivity.java
+++ smallrecipe/src/main/java/com/cn/smallrecipe/activity/LoginActivity.java
@@ -3,6 +3,8 @@ package com.cn.smallrecipe.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.cn.smallrecipe.MyActivity;
 import com.cn.smallrecipe.R;
@@ -10,17 +12,56 @@ import com.cn.smallrecipe.R;
 /**
  * Created by Administrator on 2016/2/25.
  */
-public class LoginActivity extends MyActivity {
+public class LoginActivity extends MyActivity implements View.OnClickListener {
+    private LinearLayout layout_register;
+    private LinearLayout layout_login_back;
+    private static final int RESULT_CODE = 0x01;
+    private static final int REQUEST_CODE = 0x02;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "inter LoginActivity view");
         setContentView(R.layout.activity_login);
+        findId();
+        setOclick();
+    }
+
+    private void setOclick() {
+        layout_register.setOnClickListener(this);
+        layout_login_back.setOnClickListener(this);
+    }
+
+    private void findId() {
+        layout_register = (LinearLayout) findViewById(R.id.layout_register);
+        layout_login_back = (LinearLayout) findViewById(R.id.layout_login_back);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "LoginActivity was destroyed");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.layout_register:
+                //跳入注册页面
+                startActivityForResult(new Intent(LoginActivity.this, RegisterActivity.class), REQUEST_CODE);
+                break;
+            case R.id.layout_login_back:
+                setResult(RESULT_CODE);
+                this.finish();
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            //从注册页面返回
+            Log.d(TAG, "onActivityResult-LoginActivity,requestCode:" + requestCode + ",resultCode:" + resultCode + ",data:" + data);
+        }
     }
 }
--- smallrecipe/src/main/java/com/cn/smallrecipe/activity/RegisterActivity.java
+++ smallrecipe/src/main/java/com/cn/smallrecipe/activity/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.cn.smallrecipe.activity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.cn.smallrecipe.MyActivity;
 import com.cn.smallrecipe.R;
@@ -9,11 +11,34 @@ import com.cn.smallrecipe.R;
 /**
  * Created by Administrator on 2016/2/25.
  */
-public class RegisterActivity extends MyActivity {
+public class RegisterActivity extends MyActivity implements View.OnClickListener {
+    private LinearLayout layout_register_back;
+    private static final int RESULT_CODE = 0x01;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "inter register activity");
         setContentView(R.layout.activity_register);
+        findId();
+        setOclick();
+    }
+
+    private void setOclick() {
+        layout_register_back.setOnClickListener(this);
+    }
+
+    private void findId() {
+        layout_register_back = (LinearLayout) findViewById(R.id.layout_register_back);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.layout_register_back:
+                setResult(RESULT_CODE);
+                this.finish();
+                break;
+        }
     }
 }
