3306a73b74b5a652a79a1adf31a2acc71f078573
Administrator <wenyun.zhao@ic-pay.com>
2016-03-07 23:02:34 +0800

    首次上传项目

--- libs_xxutils/src/main/java/cn/com/xxutils/likebutton/OnLikeListener.java
+++
@@ -1,9 +0,0 @@
-package cn.com.xxutils.likebutton;
-
-/**
- * Created by Joel on 23/12/2015.
- */
-public interface OnLikeListener {
-    void liked(XXLikeButton XXLikeButton);
-    void unLiked(XXLikeButton XXLikeButton);
-}
