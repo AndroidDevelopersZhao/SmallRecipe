000cd16482e9a92db649b9164f6096822ab86f36
wenyun.zhao@ic-pay.com <ai930225>
2016-03-08 10:33:34 +0800

    接入友盟推送，已完成app端代码，可通过umeng后台进行推送

--- smallrecipe/src/main/java/com/cn/smallrecipe/MyActivity.java
+++ smallrecipe/src/main/java/com/cn/smallrecipe/MyActivity.java
@@ -6,6 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.tencent.tauth.Tencent;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.MsgConstant;
+import com.umeng.message.PushAgent;
+import com.umeng.message.UmengRegistrar;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,13 +22,27 @@ import cn.smssdk.SMSSDK;
  */
 public class MyActivity extends AppCompatActivity {
     public static final String TAG = "SmallRecipe";
-    public static boolean LOGIN_STATE =false;//登陆状态，默认为false
-    public static Tencent TENCENT=null;
+    public static boolean LOGIN_STATE = false;//登陆状态，默认为false
+    public static Tencent TENCENT = null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "The parent class is inherited one times," + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         super.onCreate(savedInstanceState);
 
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+        mPushAgent.setDebugMode(true);
+        mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);//sdk开启通知声音
+        mPushAgent.onAppStart();
+        mPushAgent.enable(new IUmengRegisterCallback() {
+            @Override
+            public void onRegistered(String s) {
+                Log.w(TAG, "推送回调：" + s);
+            }
+        });
+//        PushAgent.getInstance(this).onAppStart();
+        String device_token = UmengRegistrar.getRegistrationId(this);
+        Log.w(TAG, "友盟推送已成功开启");
 
     }
 
